10afb236cf5c95f253613e2df4f0f5e8f2b52a7a
안준헌 <frozengrape@mz.co.kr>
2023-08-31 11:10:44 +0900

    safelist text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {},
+        },
     },
-  },
-  plugins: [
-
-  ],
+    plugins: [],
+    safelist: [
+        'text-3xl',
+        'text-5xl',
+        'w-1/2',
+        'w-1/3',
+        'w-1/5',
+    ]
 }
